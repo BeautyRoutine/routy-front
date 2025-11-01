89d332e5d455b27a13d256eb98bfce378b8f259c
모란희 <ranhuy.mo@gmail.com>
2025-11-01 18:18:27 +0900

    Feature/#1 admin main (#3)
    * feat: 관리자페이지 메인화면 구성
    
    사이드바, 컨텐츠 영역 분리
    부트스트랩 기반 디자인 적용
    기능명세서에 명시된 카테고리 추가
    
    resolves: #2
    
    * fix: Home 컴포넌트만 뜨는 이슈 수정
    
    BrowserRouter->HashRouter로 변경되면서 Home 컴포넌트만 뜨던 이슈 수정
    
    resolves: #2

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import logo from '../../logo.svg';
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Routy Front Ready 🚀</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+};
+
+export default Home;
