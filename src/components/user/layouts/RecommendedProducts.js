f31ba0dc4d83b180ae00831bf945991795688aaa
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-06 14:36:35 +0900

    [fix] 다시 수정

--- src/components/user/layouts/RecommendedProducts.js
+++ src/components/user/layouts/RecommendedProducts.js
@@ -10,7 +10,6 @@ const RecommendedProducts = () => {
 
   // 더미 데이터 (백엔드 연결 전) 아래 주석은 백엔드 연결 후 제거 예정
   // eslint-disable-next-line no-unused-vars
-
   const [products, setProducts] = useState([
     {
       id: 1,
