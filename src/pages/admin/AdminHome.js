a02739b64ba3d943cbf3acec274cbd5ec3e6cd80
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-20 16:19:59 +0900

    [feat] ProductAdd , ProductList, delete기능 80% 완성

--- src/pages/admin/AdminHome.js
+++ src/pages/admin/AdminHome.js
@@ -7,6 +7,12 @@ import Sidebar from 'components/admin/layouts/SideBar';
 import OrderList from 'components/admin/orders/OrderList';
 import OrderDetail from 'components/admin/orders/OrderDetail';
 
+import ProductList from 'features/products/ProductList';
+import ProductDetail from 'features/products/ProductDetail';
+import ProductAdd from 'features/products/ProductAdd';
+import ProductEdit from 'features/products/ProductEdit';
+import IngredientList from 'features/products/IngredientList';
+
 const AdminHome = () => {
   return (
     <div className="d-flex">
@@ -14,7 +20,12 @@ const AdminHome = () => {
       <div className="admin-content flex-grow-1">
         <Routes>
           {/* 상품 관리 */}
-          <Route path="productList" />
+          <Route path="productList" Component={ProductList} />
+          <Route path="products/detail/:prdNo" element={<ProductDetail />} />
+          <Route path="products/edit/:prdNo" element={<ProductEdit />} />
+          <Route path="products/add" element={<ProductAdd />} />
+          <Route path="productIng" Component={IngredientList} />
+
           {/* 주문 관리 */}
           <Route path="orderList" Component={OrderList} />
           <Route path="orderList/:odNo" Component={OrderDetail} />
