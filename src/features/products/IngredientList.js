b63154faff879470122fded988f16bca60c947ae
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-20 17:41:58 +0900

    [fix] 네비게이트 주석처리

--- src/features/products/IngredientList.js
+++ src/features/products/IngredientList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 
 const IngredientList = () => {
