e9f37385de1fd244c9078f075e4a289bc6d695b6
yejoo yoon <yoonye1111@gmail.com>
2025-12-10 16:41:36 +0900

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setUser } from '../features/user/userSlice';
-import api, { getKakaoUrl } from '../lib/apiClient';
+import api from '../lib/apiClient';
+
 
 const LoginPage = () => {
   const navigate = useNavigate();
