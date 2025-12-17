b34762533c15917d096acc298393bb854373c3c6
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-12-17 15:31:15 +0900

    feat 더 나은 서비스 추가

---
+++ src/pages/UnderConstruction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UnderConstruction = () => {
+  return (
+    <div>
+      <h1>해당 기능은 현재 준비 중입니다.</h1>
+      <p>더 나은 서비스로 곧 찾아뵙겠습니다.</p>
+      <h1>Under Construction</h1>
+      <p>This page is currently under construction.</p>
+    </div>
+  );
+};
+
+export default UnderConstruction;
