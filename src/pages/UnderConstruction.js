d42581c74900796f7d7a02f11edceee1a21aadb7
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-12-17 17:19:47 +0900

    qwer

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -4,6 +4,8 @@ const UnderConstruction = () => {
   return (
     <div>
       <h1>해당 기능은 현재 준비 중입니다.</h1>
+      <br />
+      <br></br>
       <p>더 나은 서비스로 곧 찾아뵙겠습니다.</p>
       <h1>Under Construction</h1>
       <p>This page is currently under construction.</p>
